3f6fb00ecd5f953bccc3e9f98d75273d9f555064
Ankansingh97 <ankansingh97@gmail.com>
2026-01-28 17:27:54 +0530

    new file ques

---
+++ dsa.js
@@ -0,0 +1,90 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+
+// function secondlargest(arr){
+//     let largest = -Infinity;
+//     let second_largest = -Infinity;
+    
+//     for(let i = 0 ; i < arr.length ; i++){
+//         if(arr[i] > largest){
+//              largest = arr[i] ;
+//              largest = largest ;
+//         }else if(arr[i] < largest && arr[i] > second_largest){
+//             second_largest = arr[i];
+//         }
+    
+//     }  return ([largest , second_largest]);
+    
+// }
+
+// console.log(secondlargest([1,2,3,4,5,6,7,8,9]))
+
+
+
+// function largestandsmallest(arr){
+//     let smallest = Infinity;
+//     let largest = -Infinity;
+    
+//     for(let i = 0 ; i < arr.length ; i++){
+//         if(arr[i] > largest){
+//             largest = arr[i];    
+//         }
+//         if(arr[i] < smallest){
+//             smallest = arr[i];
+//         }
+//     }
+//     return [largest, smallest];
+// }
+
+// console.log(largestandsmallest([4,5,6,7,8,9]));
+
+
+// function accendingarray(arr){
+//       for(let i = 0 ; i < arr.length -1 ; i++){
+//           for(let j = i+1 ; j < arr.length ; j++  ){
+//               if(arr[i] > arr[j]){
+//               let temp = 0;
+//               temp = arr[i];
+//               arr[i] = arr[j];
+//               arr[j] = temp;
+//           }
+         
+//           }
+//       }return(arr)
+// }
+
+
+
+console.log(decendingarray([21,2,3,14,5,46]))
+
+
+
+
+// function decendingarray(arr){
+//     for(let i = 0 ; i < arr.length - 1 ; i++){
+//         for(let j = i+1 ; j < arr.length ; j++){
+//             if(arr[i]< arr[j]){
+//                 let temp = arr[i];
+//                 arr[i] = arr[j];
+//                 arr[j] = temp;
+//             }
+//         }
+//      }return(arr)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("Try programiz.pro");
